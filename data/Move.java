57791d535a058d7f03b2c5900c514dedae4b4f13
Suchit Kumar <59784290+voxer03@users.noreply.github.com>
2022-01-04 21:16:04 -0800

    Delete data directory

--- data/Move.java
+++
@@ -1,6 +0,0 @@
-package tictactoe;
-
-public class Move {
-	public int ROW;
-	public int COL;
-}
--- data/Player.java
+++
@@ -1,6 +0,0 @@
-package tictactoe;
-
-public class Player {
-	public static final String PLAYER="X";
-	public static final String COMPUTER="O";
-}
